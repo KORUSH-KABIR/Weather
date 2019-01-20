e7fb50f91d83b609ebad76da182f85ac3c45b2a4
Mohammad Ali Riazati <mohammadali.riazati@yahoo.com>
2019-01-20 13:49:08 +0330

    initial commit
    initial commit

--- app/src/main/java/ir/iran/weather/adapter/ForecastsAdapter.java
+++ app/src/main/java/ir/iran/weather/adapter/ForecastsAdapter.java
@@ -23,24 +23,27 @@ import ir.iran.weather.weatherAPI.models.Forecasts;
 public class ForecastsAdapter extends RecyclerView.Adapter<ForecastsAdapter.ViewHolder> {
 
     private Context context;
-    private List<Forecasts> models;
+    private List<Forecasts> models; // ساخت یک لیست از مقادیر آب و هوای آینده
     private LayoutInflater layoutInflater;
 
     public ForecastsAdapter(Context context , YahooWeatherAPI api) {
         this.context = context;
-        this.models = api.getResult().getForecasts();
+        this.models = api.getResult().getForecasts(); // گرفتن لیست از api
         this.layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
     }
 
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        // تنظیم لایه ای که باید برای هر سطر نمایش دهد
         View view = layoutInflater.inflate(R.layout.layout_rec_forecast , parent , false);
         return new ViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+        // // گرفتن اطلاعات از لیست و نمایش آن روی ویو ها
 
         String tempHighValue = String.valueOf(models.get(position).getHigh());
         String tempLowValue  = String.valueOf(models.get(position).getLow());
@@ -58,10 +61,18 @@ public class ForecastsAdapter extends RecyclerView.Adapter<ForecastsAdapter.View
                 )));
     }
 
+    /**
+     * وظیفه تبدیل تاریخ از unixTime به قابل نمایش برای کاربر
+     */
+    @SuppressLint("SimpleDateFormat")
     private String getDate(long time){
+
         Date date = new Date(time * 1000L);
-        @SuppressLint("SimpleDateFormat") SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+
         simpleDateFormat.setTimeZone(TimeZone.getTimeZone("GMT+03:30"));
+
         return simpleDateFormat.format(date);
     }
 
